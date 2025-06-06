87b57764ce00f895e07f8b8baf53a1d6ede66ad8
Brendan Brown <brendan94brown@gmail.com>
2025-06-06 11:59:08 -0700

    Set up Express server with CORS and dotenv

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
