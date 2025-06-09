bf29b12632e3d84a8f905687c8a0a2bbbc16b2d4
Brendan Brown <brendan94brown@gmail.com>
2025-06-09 14:13:57 -0700

    Import Header component in App; hardcode temporary user for testing; mount Header at top of page

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import Login from "./components/Login";
 import Main from "./components/Main";
 import Modal from "./components/Modal";
 import User from "./components/User";
+import Header from "./components/Header";
 import "./styles/main.scss";
 
 function App() {
   const [apiMessage, setApiMessage] = useState("");
+  const user = "User 1"; //TEMPORARY HARDCORE FOR TESTING
 
   useEffect(() => {
     fetch("http://localhost:8080/api/test")
@@ -19,6 +21,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Header user={user} />     {/* renders Header at the top */}
         <Routes>
           <Route path="/login" element={<Login />} />
           {/* Add other routes here, e.g. */}
