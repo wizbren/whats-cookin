e6fa781ce7b1bd447eb84065987c1d6b00aea462
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-06-09 13:08:47 -0700

    login page renders,, installed react dom files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
-import { useEffect, useState } from 'react';
-import Login from './components/Login';
-import Main from './components/Main';
-import Modal from './components/Modal';
-import User from './components/User';
-import './styles/main.scss';
-
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Main from "./components/Main";
+import Modal from "./components/Modal";
+import User from "./components/User";
+import "./styles/main.scss";
 
 function App() {
-  const [apiMessage, setApiMessage] = useState('');
+  const [apiMessage, setApiMessage] = useState("");
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/test')
-      .then(res => res.json())
-      .then(data => setApiMessage(data.message))
-      .catch(err => console.error('Error fetching API:', err));
+    fetch("http://localhost:8080/api/test")
+      .then((res) => res.json())
+      .then((data) => setApiMessage(data.message))
+      .catch((err) => console.error("Error fetching API:", err));
   }, []);
 
   return (
-    <div className='App' >
-      <h1>What's Cookin'</h1>
-      <p>Message from API: {apiMessage}</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {/* Add other routes here, e.g. */}
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
