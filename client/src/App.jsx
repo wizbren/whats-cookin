f97f389aefad4b1f7657774b9fc2eacf08108f51
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-06-09 12:21:46 -0700

    skeleton structure built for app.jsx and mmain.jsx to render app

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from './components/Login';
+import Main from './components/Main';
+import Modal from './components/Modal';
+import User from './components/User';
+import '../styles/main.scss';
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+};
+
+export default App;
