970baecc2e3fce2ab43a5da560f6dc2895513430
BoxinCao <ngxmex@gmail.com>
2025-06-10 13:25:55 -0400

    mock data configured

---
+++ server/src/db/seeds/mockdata.sql
@@ -0,0 +1,19 @@
+
+
+INSERT INTO Users (name, contact_number)
+VALUES
+  ('John Doe', '123-456-7890'),
+  ('Jane Smith', '987-654-3210'),
+  ('Alice Johnson', '555-555-5555'),
+  ('Bob Lee', '444-444-4444'),
+  ('Charlie Brown', '333-333-3333');
+
+INSERT INTO Recipes (name, photo_url, description, price, is_active, processing_time)
+VALUES
+  ('Salad', 'https://marathonmouth.me/wp-content/uploads/2021/07/community-foods-8.jpg?w=1024', 'Fresh garden salad with dressing', 5.75, true, 10),
+  ('Pasta', 'https://pngimg.com/d/pasta_PNG70.png', 'Spaghetti with tomato basil sauce', 10.25, true, 20),
+  ('Garlic Bread', 'https://png.pngtree.com/png-vector/20240607/ourmid/pngtree-garlic-bread-concepts-png-image_12638044.png', 'Toasted garlic bread slices', 3.50, true, 5),
+  ('Soda', 'https://banner2.cleanpng.com/20180716/bwu/aavh2vvhd.webp', 'Chilled soft drink can', 1.99, true, 5),
+  ('Burger', 'https://e7.pngegg.com/pngimages/201/77/png-clipart-delicious-beef-burger-delicious-beef-burger-tomato-thumbnail.png', 'Delicious beef burger filled with goodness!', 1.68, true, 30);
+
+
