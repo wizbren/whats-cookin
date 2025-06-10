45a02d290c1208e6ad0d80c26dc852d4e9ab4131
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-06-09 20:28:12 -0700

    began work on hooks folder to handle data requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,22 @@
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Main from "./components/Main";
-import ModalComponent from './components/Modal';
+import ModalComponent from "./components/Modal";
 import Header from "./components/Header";
+import useAppData from "./hooks/useAppData";
 import "./styles/main.scss";
 
 function App() {
-  const [apiMessage, setApiMessage] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
   const user = "User 1"; //TEMPORARY HARDCORE FOR TESTING
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/test")
-      .then((res) => res.json())
-      .then((data) => setApiMessage(data.message))
-      .catch((err) => console.error("Error fetching API:", err));
-  }, []);
+  const { apiMessage, modalOpen, setModalOpen } = useAppData();
 
   return (
     <Router>
       <div className="App">
-        <Header user={user} />     {/* renders Header at the top */}
+        <Header user={user} /> {/* renders Header at the top */}
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main apiMessage={apiMessage} />} />
         </Routes>
         <ModalComponent show={modalOpen} onClose={() => setModalOpen(false)} />
       </div>
---
+++ client/src/hooks/useAppData.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useAppData = () => {
+  const [apiMessage, setApiMessage] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/test")
+      .then((res) => res.json())
+      .then((data) => setApiMessage(data.message))
+      .catch((err) => console.error("Error fetching API:", err));
+  }, []);
+
+  return {
+    apiMessage,
+    modalOpen,
+    setModalOpen,
+  };
+};
+
+export default useAppData;
