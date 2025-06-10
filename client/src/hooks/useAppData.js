ae1dbc6a31c1e22b2f7d93c7cc911199bd2cbf72
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-06-10 13:13:50 -0700

    likes should now make changes to the database, verification of recipe owner will need to be done in database so deletes dont delete other users likes

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -14,16 +14,47 @@ const useAppData = () => {
   }, []);
 
   //handles state of liked recipes and should therefore update the DB/API too
-  const toggleLikedStatus = (recipeId) => {
-    setLikedStatus((prev) => {
-      const currentStatus = prev[recipeId] || "notLiked";
-      const updatedStatus = currentStatus === "liked" ? "notLiked" : "liked";
+  // when called later call like this toggleLikedStatus(recipe.id, recipe.url, recipe.image)
+  const toggleLikedStatus = (recipeId, url, image) => {
+    const currentStatus = likedStatus[recipeId] || "notLiked";
+    const updatedStatus = currentStatus === "liked" ? "notLiked" : "liked";
 
-      return {
-        ...prev,
-        [recipeId]: updatedStatus,
-      };
-    });
+    setLikedStatus((prev) => ({
+      ...prev,
+      [recipeId]: updatedStatus,
+    }));
+
+    if (updatedStatus === "liked") {
+      //if status is liked, it gets added
+      fetch("http://localhost:8080/api/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: userId,
+          url,
+          image,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log("Liked recipe saved:", data))
+        .catch((err) => console.error("Error saving recipe:", err));
+    } else {
+      fetch(`http://localhost:8080/api/recipes/${recipeId}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user_id: userId }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Failed to delete recipe");
+          }
+          return res.json();
+        })
+        .then((data) => console.log("Recipe deleted:", data))
+        .catch((err) => console.error("Error deleting recipe:", err));
+    }
   };
 
   //handles state of logged in user by user id
