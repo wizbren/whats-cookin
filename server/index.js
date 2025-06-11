f852a968b6f2621bb8eea30e6688695ee37cb98d
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-06-10 17:26:28 -0700

    seems to look like maybe it would work, going to use it to test

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
-
+const { Pool } = require('pg');
+const pool = new Pool();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -11,6 +11,22 @@ app.use(express.json());
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from Express!' });
+});
+
+app.get('/api/users/:id', async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const userResult = await pool.query('SELECT * FROM Users WHERE id = $1', [userId]);
+    const recipeResult = await pool.query('SELECT * FROM Recipes WHERE user_id = $1', [userId]);
+
+    const user = userResult.rows[0];
+    const recipes = recipeResult.rows;
+
+    res.json({ user, recipes });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 
 app.listen(PORT, () => {
